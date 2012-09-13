2d25aedc75b35de8b2fa38aebbc3b8d4cf1f6395
Scott González <scott.gonzalez@gmail.com>
2012-09-13 13:00:31 -0400

    Upgraded grunt-jquery-content to 0.5.x.

--- entries2html.xsl
+++ entries2html.xsl
@@ -1,6 +1,6 @@
 <xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0">
 
-<xsl:import href="entries2html-base.xsl"/>
+<xsl:import href="node_modules/grunt-jquery-content/tasks/jquery-xml/entries2html-base.xsl"/>
 <xsl:variable name="method-prefix-dot" select="false()"/>
 
 <xsl:template name="example-code">
--- grunt.js
+++ grunt.js
@@ -15,7 +15,7 @@ grunt.initConfig({
 		grunt: "grunt.js"
 	},
 	xmllint: {
-		all: [].concat( entryFiles, "cat2tax.xsl", "categories.xml", "entries2html.xsl", "xml2json.xsl" )
+		all: [].concat( entryFiles, "categories.xml", "entries2html.xsl" )
 	},
 	xmltidy: {
 		all: [].concat( entryFiles, "categories.xml" )
@@ -24,7 +24,7 @@ grunt.initConfig({
 		all: entryFiles
 	},
 	"build-resources": {
-		all: grunt.file.expandFiles( "resources/*" )
+		all: grunt.file.expandFiles( "resources/**" )
 	},
 	wordpress: grunt.utils._.extend({
 		dir: "dist/wordpress"
@@ -32,7 +32,8 @@ grunt.initConfig({
 });
 
 grunt.registerTask( "default", "build-wordpress" );
-grunt.registerTask( "build-wordpress", "clean lint xmllint build-xml-entries build-xml-categories build-resources" );
+grunt.registerTask( "build", "build-xml-entries build-xml-categories build-xml-full build-resources" );
+grunt.registerTask( "build-wordpress", "clean lint xmllint build" );
 grunt.registerTask( "tidy", "xmllint xmltidy" );
 
 };
